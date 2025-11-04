7772d1634b6292a958159492ef8850d5fd6b031f
Omar Youssef <omar.youssef@flairstech.com>
2025-11-04 16:34:07 +0200

    intial

---
+++ src/main/java/com/screen/quickprint/QuickPrintApplication.java
@@ -0,0 +1,13 @@
+package com.screen.quickprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickPrintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/screen/quickprint/QuickPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.screen.quickprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
